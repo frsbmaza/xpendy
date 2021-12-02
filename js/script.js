7f9c58ffe6a813554edc45e2eab8cb1f03739253
Fajle Rabbi <frsbmaza2016@gmail.com>
2021-12-02 16:57:21 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import lottieWeb from 'https://cdn.skypack.dev/lottie-web';
+
+var animation = lottieWeb.loadAnimation({
+  container: document.getElementById('animate'),
+  path: 'https://cdn.shopify.com/s/files/1/0171/1613/5478/t/15/assets/animation.json',
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  name: "Demo Animation" });
